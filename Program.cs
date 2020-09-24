67a654ffc87fca49eaad89cad093a8de71b1610c
ahills1 <70347865+ahills1@users.noreply.github.com>
2020-09-24 11:55:20 -0400

    Commit the change

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Programmer: Ashley Hills
+// Project: Tech Project 3b
+// Date: 09/24/2020
+// Description: A console program that demonstrates the use of an Auto-Populated Array 
+using System;
+
+namespace Tech_Project_3b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = new[] {  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+            for (int i = 1; i < 26; i++)
+            {
+                Console.WriteLine("Element Value = " + Array[i]);
+
+                if (Array[i] == 25)
+                {
+                    Console.WriteLine("Array interations have ended...");
+                    Console.WriteLine("Press any key to exit this program...");
+                }
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
